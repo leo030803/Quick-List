fb33dc21af6c80c80cf5a74581ed840951e73a10
Leonardo <leo.braga@eaportal.org>
2025-05-31 12:57:03 -0300

    criação da função para marcar os itens e adição do evento de click no label

---
+++ main.js
@@ -0,0 +1,68 @@
+const items = []
+
+function addItem() {
+
+    const itemName = document.querySelector("#item").value
+
+    const item = {
+
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+function showItemsList() {
+
+    const sectionList = document.querySelector(".list")
+
+    sectionList.innerHTML = ""
+
+    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+
+     
+    items.map((item, index) => {
+
+        sectionList.innerHTML += `
+        
+        <div class="item">
+
+        <div>
+
+        <input type="checkbox" name="list" id="item-${index}" ${item.checked === true ? "checked" : ""}>
+        <div class="custom-checkbox">
+
+        <img src="./assets/checked.svg" alt="checked">
+        
+        </div>
+        <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+        </div>
+        <button>
+
+        <img src="./assets/trash-icon.svg" alt="trash icon">
+        </button>
+
+        </div>
+        
+        
+        `
+   
+        
+    })
+}
+
+function checkItem(itemName) {
+
+    const item = items.find((item) => item.name === itemName)
+    item.checked = !item.checked
+    showItemsList()
+}
+
+
+
+        
